3021524696bf8359bb1a15243e7cc2dc876c48c0
SovaV <sova.volodymyr95@gmail.com>
2021-10-12 17:30:01 +0300

    sub

---
+++ src/component/App copy.js
@@ -0,0 +1,116 @@
+// import React, { Component, useEffect, useState } from 'react';
+// import Container from '../component/Container/Container';
+// import shortid from 'shortid';
+
+// //====================================
+// import ContactList from './ContactList/ContactList ';
+// import ContactForm from './ContactForm/ContactForm';
+// import Filter from './Filter/Filter';
+
+// const useLocalStorage = (id) => {
+//    const [contacts, setContacts] = useState(() => {
+//     return JSON.parse(window.localStorage.getItem('id')) ??
+//     [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]
+//    })
+//     useEffect(() => {
+//     window.localStorage.setItem('id', JSON.stringify(contacts))
+//   },[id, contacts] )
+//   return [contacts, setContacts]
+// }
+
+// export default function App() {
+//   const [name, setName] = useLocalStorage('name', '')
+//  const [ number, setNumber] = useLocalStorage('number' , '')
+
+//   useEffect(() => {
+//     window.localStorage.setItem('contacts', JSON.stringify(contacts))
+//   }, )
+
+//   return (
+//       <Container>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.addContact} />
+//         <h2>Contacts</h2>
+//         <Filter value={this.filter} onChange={this.changeFilter} />
+//         <ContactList contacts={visibleContacts} onDeleteContact={this.deleteContact} />
+//       </Container>
+//     );
+// }
+
+// class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+//   //============================================
+//   addContact = ({ name, number }) => {
+//     const obj = this.state.contacts;
+//     const contact = {
+//       id: shortid.generate(),
+//       name,
+//       number,
+//     };
+//     const objectContacts = obj.some(contact => contact.name.toLowerCase() === name.toLowerCase());
+//     if (objectContacts) {
+//       alert(`${name} is already in contacts`);
+//     } else {
+//       this.setState(({ contacts }) => ({
+//         contacts: [contact, ...contacts],
+//       }));
+//     }
+//   };
+
+//   deleteContact = contactId => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+//     }));
+//   };
+//   //============================================
+//   //============================================
+//   changeFilter = e => {
+//     this.setState({ filter: e.currentTarget.value });
+//   };
+//   getVisibleContacts = () => {
+//     const { filter, contacts } = this.state;
+//     const normalizedFilter = filter.toLowerCase();
+
+//     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+//   };
+//   //=====LOCAL STORAGE=============================================
+//   componentDidMount() {
+//     const contacts = localStorage.getItem('contacts');
+//     const parsedContacts = JSON.parse(contacts);
+//     if (parsedContacts) {
+//       this.setState({ contacts: parsedContacts });
+//     }
+//   }
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+//   //================================================
+//   //=================================================
+//   render() {
+//     const visibleContacts = this.getVisibleContacts();
+//     return (
+//       <Container>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.addContact} />
+//         <h2>Contacts</h2>
+//         <Filter value={this.filter} onChange={this.changeFilter} />
+//         <ContactList contacts={visibleContacts} onDeleteContact={this.deleteContact} />
+//       </Container>
+//     );
+//   }
+// }
+
+// export default App;
--- src/component/ContactForm/ContactForm.js
+++ src/component/ContactForm/ContactForm.js
@@ -1,64 +1,66 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import p from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-  state = {
-    name: '',
-    number: '',
-  };
+export default function ContactForm({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  hendleChange = e => {
+  const hendleChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
 
-  hendleSubmit = e => {
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
+  };
+  const hendleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
+    onSubmit({ name, number });
+    reset();
+  };
+  const reset = () => {
+    setName('');
+    setNumber('');
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-  render() {
-    return (
-      <form className={p.box} onSubmit={this.hendleSubmit}>
-        <label className={p.wrapp}>
-          <p>Name</p>
-          <input
-            value={this.state.name}
-            onChange={this.hendleChange}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-          />
-        </label>
-        <label className={p.wrapp}>
-          <p>Number</p>
-          <input
-            value={this.state.number}
-            onChange={this.hendleChange}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-          />
-        </label>
-        <br />
-        <button className={p.btn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form className={p.box} onSubmit={hendleSubmit}>
+      <label className={p.wrapp}>
+        <p>Name</p>
+        <input
+          value={name}
+          onChange={hendleChange}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+      </label>
+      <label className={p.wrapp}>
+        <p>Number</p>
+        <input
+          value={number}
+          onChange={hendleChange}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+        />
+      </label>
+      <br />
+      <button className={p.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
-
-export default ContactForm;
